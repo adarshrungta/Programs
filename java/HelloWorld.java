507cbf7e5fcddaf692a07368eec7128f0409f143
roshanDaruka <r.daruka@yahoo.co.in>
2017-12-21 11:50:56 +0530

    hello world

---
+++ java/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        int n=1;
+        int i=0;
+        while (i<=1000)
+        {
+            if(n%2==0){
+            System.out.println(n);
+            }
+            i++;
+            n++;
+        }
+    }
+}
